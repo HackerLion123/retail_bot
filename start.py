e43fec5b455b224097408fc7bbb927c85a7df193
HackerLion123 <s.bharath64@gmail.com>
2024-05-30 18:17:08 +0530

    [main] adding inital code.

---
+++ start.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+sys.path.append(os.path.dirname(CURRENT_DIR))
+
+
+os.environ["PYTHONPATH"] = CURRENT_DIR
+os.environ["PYTHON_PATH"] = CURRENT_DIR
+os.environ["ENVIRONMENT_MIN"] = "DEV"
