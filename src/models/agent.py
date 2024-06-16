dc9f20291cfd029642ab1a48f5ce7e63a4e9fa5d
HackerLion123 <s.bharath64@gmail.com>
2024-06-16 12:46:21 +0530

    [main] added code files.

---
+++ src/models/agent.py
@@ -0,0 +1,10 @@
+class ChatAgent:
+
+    def __init__(self) -> None:
+        pass
+
+    def build(self):
+        pass
+
+    def chat(self):
+        pass
---
+++ src/models/rag.py
@@ -0,0 +1,13 @@
+from langchain.vectorstores import Chroma
+
+
+class RAGSearch:
+
+    def __init__(self) -> None:
+        pass
+
+    def load_db(self):
+        pass
+
+    def search(self):
+        pass
