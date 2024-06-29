be1aa5efa6b60619ec986d71ee72a5fc38d73aaa
HackerLion123 <s.bharath64@gmail.com>
2024-06-29 16:01:38 +0530

    [main] adding the lastest chnages.

--- src/models/agent.py
+++ src/models/agent.py
@@ -1,9 +1,13 @@
+from langchain_community.llms import Ollama
+from langchain.agents import AgentType
+
+
 class ChatAgent:
 
     def __init__(self) -> None:
         pass
 
-    def build(self):
+    def build(self, prompt):
         pass
 
     def chat(self):
---
+++ src/models/chat.py
@@ -0,0 +1,4 @@
+from langchain import PromptTemplate
+from langchain.output_parsers import StructuredOutputParser
+
+from src.models.agent import ChatAgent
