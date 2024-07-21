35cfb634b463ba85fce67d4046b69456ef65091e
HackerLion123 <s.bharath64@gmail.com>
2024-07-21 16:07:35 +0530

    [main] added custom agent.

--- src/models/agent.py
+++ src/models/agent.py
@@ -1,7 +1,7 @@
 from langchain.memory import ChatMessageHistory, ConversationBufferMemory
 from langchain_community.tools import DuckDuckGoSearchRun
-from langchain_community.chat_models import ChatOllama
-from langchain.agents import AgentExecutor, create_react_agent
+from langchain_community.llms import Ollama
+from langchain.agents import AgentExecutor, initialize_agent
 
 
 from src import config
@@ -13,18 +13,34 @@ class ChatAgent:
         self.messages = []
         self.rag_chain = None
         self.memory = ConversationBufferMemory(
-            memory_key="history", return_messages=True
+            memory_key="history", return_messages=True, output_key="output"
         )
 
     def build(self, prompt):
-        self._define_model()
+        self._create_agent(prompt=prompt)
 
-    def _create_prompt(self):
-        self.prompt = """
-        """
+    def _create_agent(self, prompt):
+        llm = Ollama(**config.MODEL_CONFIG)
+        tools = [DuckDuckGoSearchRun()]
+        self.agent = initialize_agent(
+            agent="chat-conversational-react-description",
+            tools=tools,
+            llm=llm,
+            verbose=True,
+            early_stopping_method="generate",
+            memory=self.memory,
+            handle_parsing_errors=True,
+        )
+        self.agent_executor = AgentExecutor(
+            agent=self.agent, tools=tools, verbose=True, handle_parsing_errors=True
+        )
 
-    def _define_model(self):
-        llm = ChatOllama(**config.MODEL_CONFIG)
+    def chat(self, question):
+        return self.agent_executor.invoke(question)
 
-    def chat(self):
-        pass
+
+if __name__ == "__main__":
+    agent = ChatAgent()
+
+    agent.build("You are a style bot you provide style suggestions")
+    agent.chat([("human", "Tropical trend")])
--- start.py
+++ start.py
@@ -9,4 +9,4 @@ sys.path.append(os.path.dirname(CURRENT_DIR))
 
 os.environ["PYTHONPATH"] = CURRENT_DIR
 os.environ["PYTHON_PATH"] = CURRENT_DIR
-os.environ["ENVIRONMENT_MIN"] = "DEV"
+os.environ["ENVIRONMENT"] = ""
