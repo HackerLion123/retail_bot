9a963896673926fc985d14e17f4658304a853407
HackerLion123 <s.bharath64@gmail.com>
2024-07-21 19:27:21 +0530

    [main] adding lastest code.

--- src/models/chat.py
+++ src/models/chat.py
@@ -6,6 +6,20 @@ from src.models.agent import ChatAgent
 from src.models.rag import RAGSearch
 
 
+class StyleAgentPrompt:
+
+    bos = "<|begin_of_text|>"
+    header_start = "<|start_header_id|>"
+    header_end = "<|end_header_id|>"
+    eos = "<|eot_id|>"
+
+    prompt = f"""
+    {bos}{header_start} system {header_end}
+    
+
+    """
+
+
 def generate_response():
     pass
 
